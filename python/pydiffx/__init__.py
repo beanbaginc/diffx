9c217d3afb861471aae125dfa40b48b3adf28575
Christian Hammond <christian@beanbaginc.com>
2021-07-23 17:52:50 -0700

    Add convenience imports and docs to the base pydiffx module.
    This adds the most common imports to the `pydiffx` module, making it
    very easy to import `DiffX`, `DiffXReader`, `DiffXWriter`, and various
    objects holding option constants.
    
    This module has been given some basic introductory documentation and
    a reference to the DiffX spec.
    
    Testing Done:
    Tested importing from this module.
    
    Reviewed at https://reviews.reviewboard.org/r/11738/

---
+++ python/pydiffx/__init__.py
@@ -0,0 +1,41 @@
+"""An implementation of DiffX, an extensible, structured Unified Diff format.
+
+pydiffx makes it easy to work with `DiffX <https://diffx.org>`_ files.
+This is a proposed standard for a new diff format that can contain
+structured metadata, formal parsing rules, multiple commits and binary files,
+while remaining backwards-compatible with existing Unified Diff parsers.
+
+This module is the starting point for using pydiffx, and contains several
+convenience imports:
+
+.. autosummary::
+   :nosignatures:
+
+   ~pydiffx.dom.objects.DiffX
+   ~pydiffx.options.DiffType
+   ~pydiffx.options.LineEndings
+   ~pydiffx.options.MetaFormat
+   ~pydiffx.options.PreambleMimeType
+   ~pydiffx.DiffXReader
+   ~pydiffx.DiffXWriter
+"""
+
+from __future__ import unicode_literals
+
+from pydiffx.dom.objects import DiffX
+from pydiffx.options import DiffType, LineEndings, MetaFormat, PreambleMimeType
+from pydiffx.reader import DiffXReader
+from pydiffx.writer import DiffXWriter
+
+
+__all__ = (
+    'DiffType',
+    'DiffX',
+    'DiffXReader',
+    'DiffXWriter',
+    'LineEndings',
+    'MetaFormat',
+    'PreambleMimeType',
+)
+
+__autodoc_excludes__ = __all__
